0f45114a60c122490f93de16c8482b988229a186
Grzegorz Lippe <grzegorz.lippe@googlemail.com>
2020-12-21 10:20:21 +0100

    rady

--- machine_learning/02_Software_Engineering_Fundamentals/lesson_05/setup.py
+++ machine_learning/02_Software_Engineering_Fundamentals/lesson_05/setup.py
@@ -6,18 +6,14 @@ with open(path.join(this_directory, 'rki_scraper', 'README.md'), encoding='utf-8
     long_description = f.read()
 
 setup(name='rki_scraper',
-      version='0.1.3',
+      version='0.1.7',
       description='Scrape the RKI Data from Arcgis into a Pandas Dataframe',
       long_description=long_description,
       long_description_content_type='text/markdown',
       packages=['rki_scraper'],
       include_package_data=True,
       author='Grzegorz Lippe',
-      install_requires=['os',
-                        'setuptools',
-                        'pandas',
-                        'requests',
-                        'pathlib',
-                        'datetime'],
+      install_requires=['pandas',
+                        'requests'],
       author_email = 'grzegorz.lippe@posteo.de',
       zip_safe=False)
